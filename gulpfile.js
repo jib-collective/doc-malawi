771f83abf0a4a8da194568e689438ccb36b154fd
Gustav Pursche <pursche@posteo.de>
2016-06-11 15:33:53 +0200

    Add gulp AWS workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
 'use strict';
 
+const cloudfront = require('gulp-cloudfront-invalidate');
 const gulp = require('gulp');
-const gzip = require('gulp-gzip');
-const s3 = require('gulp-s3');
+const awspublish = require('gulp-awspublish');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
-const awsConfig = require('./aws.json');
+const s3Config = require('./aws.json').s3;
+const cloudfrontConfig = {
+  accessKeyId: s3Config.accessKeyId,
+  secretAccessKey: s3Config.secretAccessKey,
+  region: s3Config.region,
+  bucket: s3Config.bucket,
+  distribution: require('./aws.json').cloudfront.distributionId,
+  paths: [
+    '/malawi/*',
+  ],
+};
 
-gulp.task('upload', () => {
-  gulp.src('./de/*.html', {read: false})
-    .pipe(s3(awsConfig));
+gulp.task('styles', () => {
+  return gulp.src('./assets/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/styles/'));
+});
+
+gulp.task('upload', ['styles'], () => {
+  let publisher = awspublish.create(s3Config);
+
+  gulp.src([
+    './de/*.html',
+    './dist/**/*',
+  ])
+    .pipe(rename((path) => {
+        path.dirname = `/malawi/de/${path.dirname}`;
+        return path;
+    }))
+    .pipe(publisher.publish())
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter())
+    .pipe(cloudfront(cloudfrontConfig));
 });
